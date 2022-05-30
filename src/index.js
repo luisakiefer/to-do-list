9eefa60756a54370d52ec8895fd12395effe62f2
Luísa Kiefer <91638544+luisakiefer@users.noreply.github.com>
2022-05-30 11:21:58 -0300

    updated js file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,61 @@
-// get value from input
-// display the value inside UL
-// add a checkbox - when checked move to "Done"
-// add a delete button
+window.addEventListener("load", () => {
+  let form = document.querySelector("#new-task-form");
+  let input = document.querySelector("#new-task-input");
+  let list = document.querySelector("#tasks");
 
-function createTask() {
-  let li = document.querySelector("#task");
-  li.innerHTML = `${task.value}`;
-}
+  console.log(form);
 
-function newTask(event) {
-  event.preventDefault();
-  let task = document.querySelector("#taskToDo");
-  alert(`${task.value}`);
-  createTask(task.value);
-}
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-let addTaskButton = document.querySelector("#submitButton");
-addTaskButton.addEventListener("click", newTask);
+    let task = input.value;
+
+    let taskItem = document.createElement("div");
+    taskItem.classList.add("task");
+
+    let taskContent = document.createElement("div");
+    taskContent.classList.add("content");
+
+    taskItem.appendChild(taskContent);
+
+    let taskInput = document.createElement("input");
+    taskInput.classList.add("text");
+    taskInput.type = "text";
+    taskInput.value = task;
+    taskInput.setAttribute("readonly", "readonly");
+
+    taskContent.appendChild(taskInput);
+
+    let taskEdit = document.createElement("button");
+    taskEdit.classList.add("edit");
+    taskEdit.innerTect = "Editar";
+
+    let taskDelete = documente.createElement("button");
+    taskDelete.classList.add("delete");
+    taskDelete.innerText = "Deletar";
+
+    taskActions.appendChild(taskEdit);
+    taskActions.appendChild(taskDelete);
+
+    taskItem.appendChild(taskActions);
+
+    list.appendChild(taskItem);
+
+    input.value = "";
+
+    taskEdit.addEventListener("click", (event) => {
+      if (taskEdit.innerText.toLowerCase() == "edit") {
+        taskEdit.innerText = "Salvar";
+        taskInput.removeAttribute("readonly");
+        taskInput.focus();
+      } else {
+        taskEdit.innerText = "Editar";
+        taskInput.setAttribute("readonly", "readonly");
+      }
+    });
+
+    taskDelete.addEventListener("click", (event) => {
+      list.removeChild(taskItem);
+    });
+  });
+});
